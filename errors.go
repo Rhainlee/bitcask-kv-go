cad4f1f871085914878be83d7db1de57b4f3a727
rhainlee <imrhainlee@gmail.com>
2023-03-16 21:21:31 +0800

    数据库启动流程：加载文件&加载索引

--- errors.go
+++ errors.go
@@ -4,11 +4,9 @@ import "errors"
 
 var (
 	// ErrKeyIsEmpty 定义一个名为 ErrkeyIsEmpty 的全部变量，其类型为error
-	ErrKeyIsEmpty = errors.New("the key is empty")
-
-	ErrIndexUpdateFailed = errors.New("failed to update index")
-
-	ErrKeyNotFound = errors.New("key not found in database")
-
-	ErrDataFileNotFound = errors.New("data file is not found")
+	ErrKeyIsEmpty             = errors.New("the key is empty")
+	ErrIndexUpdateFailed      = errors.New("failed to update index")
+	ErrKeyNotFound            = errors.New("key not found in database")
+	ErrDataFileNotFound       = errors.New("data file is not found")
+	ErrDataDirectoryCorrupted = errors.New("the database directory maybe corrupted")
 )
---
+++ options.go
@@ -0,0 +1,25 @@
+package bitcask_kv_go
+
+type Options struct {
+	// 数据库数据目录
+	DirPath string
+
+	// 数据文件的大小
+	DataFileSize int64
+
+	// 持久化策略：每次写数据是否持久化
+	SyncWrites bool
+
+	// 索引类型
+	IndexType IndexerType
+}
+
+type IndexerType = int8
+
+const (
+	// Btree 索引
+	Btree IndexerType = iota + 1
+
+	// ART Adaptive Radix Tree 自适应基数树索引
+	ART
+)
